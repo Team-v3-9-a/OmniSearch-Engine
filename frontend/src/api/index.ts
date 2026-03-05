4ad8a8acd4f63e4bb2bac3e918a87f464e8007c3
Mys2018 <kirillcodminecraft@gmail.com>
2026-03-05 13:48:45 +0700

    fix(frontend): The environment variable in the index.ts file has been changed.

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = `${import.meta.env.REACT_APP_SERVER_HOST}/api/v1`
+const API_BASE_URL = `${import.meta.env.VITE_SERVER_HOST}/api/v1`
 
 export const uploadVideo = async (videoFile: FormData) => {
     const response = await axios.post(`${API_BASE_URL}/upload`, {videoFile}, {
