c06fea0db757778d912aa1a05a7e91bbf00fd6b9
Pavel Ryzhenkov <mpaasha4@gmail.com>
2026-03-16 16:36:12 -0500

    [OMNI-27] Configured Postgres and MinIO dev playgrounds

--- backend/src/main/kotlin/com/v39a/omni/plugins/DI.kt
+++ backend/src/main/kotlin/com/v39a/omni/plugins/DI.kt
@@ -6,6 +6,8 @@ import com.v39a.omni.feature.video.domain.VideoStorage
 import com.v39a.omni.feature.video.infrastructure.MinioVideoStorage
 import com.v39a.omni.feature.video.infrastructure.PostgresVideoRepository
 import io.ktor.server.application.*
+import io.minio.BucketExistsArgs
+import io.minio.MakeBucketArgs
 import io.minio.MinioClient
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
@@ -14,20 +16,35 @@ import org.koin.logger.slf4jLogger
 fun Application.configureFrameworks() {
     val videoModule = module {
         single {
-            MinioClient.builder()
-                .endpoint(System.getenv("minio.endpoint"))
-                .credentials(
-                    System.getenv("minio.accessKey"),
-                    System.getenv("minio.secretKey"),
-                )
+            val endpoint = System.getProperty("S3_ENDPOINT") ?: System.getenv("S3_ENDPOINT") ?: "http://minio:9000"
+            val accessKey = System.getProperty("S3_ACCESS_KEY") ?: System.getenv("S3_ACCESS_KEY") ?: "admin"
+            val secretKey = System.getProperty("S3_SECRET_KEY") ?: System.getenv("S3_SECRET_KEY") ?: "password123"
+
+            val client = MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
                 .build()
+
+            try {
+                val found = client.bucketExists(BucketExistsArgs.builder().bucket("videos").build())
+                if (!found) {
+                    client.makeBucket(MakeBucketArgs.builder().bucket("videos").build())
+                    println("Bucket 'videos' created successfully!")
+                } else {
+                    println("Bucket 'videos' already exists.")
+                }
+            } catch (e: Exception) {
+                println("Failed to initialize bucket: ${e.message}")
+            }
+
+            client
         }
 
         // Метод get() автоматически найдет MinioClient, зарегистрированный шагом выше
         single<VideoStorage> {
             MinioVideoStorage(
                 minioClient = get(),
-                bucket = System.getenv("MINIO_BUCKET") ?: "videos"
+                bucket = System.getenv("minio.bucket") ?: "videos"
             )
         }
 
--- backend/src/main/kotlin/com/v39a/omni/plugins/Database.kt
+++ backend/src/main/kotlin/com/v39a/omni/plugins/Database.kt
@@ -6,18 +6,23 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.UUID
 
 fun Application.configureDatabase() {
 
     val dbUrl = environment.config.property("storage.jdbcUrl").getString()
     val dbUser = environment.config.property("storage.username").getString()
     val dbPassword = environment.config.property("storage.password").getString()
+    val dbDriver = environment.config.property("storage.driverClassName").getString()
 
     val config = HikariConfig().apply {
         jdbcUrl = dbUrl
         username = dbUser
         password = dbPassword
-        driverClassName = "org.postgresql.Driver"
+        driverClassName = dbDriver
         maximumPoolSize = 3
         isAutoCommit = false
         transactionIsolation = "TRANSACTION_REPEATABLE_READ"
@@ -28,10 +33,35 @@ fun Application.configureDatabase() {
         val dataSource = HikariDataSource(config)
         Database.connect(dataSource)
         environment.log.info("Database connected successfully!")
+        transaction {
+            // CREATE TABLE IF NOT EXISTS)
+            SchemaUtils.create(VideoTable)
+
+            // Сид базы данных моками
+            if (VideoTable.selectAll().empty()) {
+
+                // Mock-video 1
+                VideoTable.insert {
+                    it[id] = UUID.randomUUID()
+                    it[fileName] = "funny_cats.mp4"
+                    it[s3Path] = "videos/funny_cats.mp4"
+                    it[status] = "READY"
+                }
+
+                // Mock-video 2
+                VideoTable.insert {
+                    it[id] = UUID.randomUUID()
+                    it[fileName] = "ml_lecture.mp4"
+                    it[s3Path] = "videos/ml_lecture.mp4"
+                    it[status] = "PROCESSING"
+                }
+
+                environment.log.info("Database seeded with mock videos!")
+            }
+        }
     } catch (e: Exception) {
         environment.log.error("! Failed to connect to database: ${e.message}")
         throw e;
     }
 
-    SchemaUtils.create(VideoTable)
 }
