290ae2f2cdacce58df6dd9b62b0722c667072c5b
Pavel Ryzhenkov <mpaasha4@gmail.com>
2026-04-21 04:19:49 -0500

    feat(Video): extended VideoStatus enum [OMNI-54]

--- backend/src/main/kotlin/com/v39a/omni/feature/video/domain/Video.kt
+++ backend/src/main/kotlin/com/v39a/omni/feature/video/domain/Video.kt
@@ -1,15 +1,21 @@
 package com.v39a.omni.feature.video.domain
 
+import kotlinx.datetime.LocalDateTime
+import java.math.BigInteger
 import java.util.UUID
 
 
-data class Video(val id: UUID, val path: String
-, val status: VideoStatus?
+data class Video(val id: UUID, val path: String, val status: VideoStatus?,
+                 val title: String,
+                 val durationSeconds: BigInteger,
+                 val thumbnailPath: String,
+                 val createdAt: LocalDateTime,
 )
 
 enum class VideoStatus {
-    UPLOADING,
-    FAILED,
     UPLOADED,
-
+    PROCESSING_MEDIA,
+    PROCESSING_ML,
+    READY,
+    ERROR,
 }
