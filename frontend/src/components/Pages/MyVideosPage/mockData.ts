5435feb42ebcd65844ed8f8f13b5be50443bbf80
Mys2018 <kirillcodminecraft@gmail.com>
2026-05-30 15:07:45 +0700

    feat(MyVideos): Создана страница "Мои видео", улучшен интерфейс и создана мобильная версия
    - Изменен дизайн
    - Убран UploadProgress.tsx, но оставлен в файлах
    - Добавлена страница "Мои Видео"
    - Добавлена адаптация для мобильных устройств
    - Убран react-tostify

---
+++ frontend/src/components/Pages/MyVideosPage/mockData.ts
@@ -0,0 +1,46 @@
+import type { MyVideoItem } from "@/types/api";
+
+export const mockData: MyVideoItem[] = [
+  {
+    "id": "04429c03-f9bb-4b57-81cc-c5f3bd40f127",
+    "title": "cafe20001-0218.mkv",
+    "status": "ERROR",
+    "createdAt": "2026-05-25T07:43:03.635634",
+    "updatedAt": "2026-05-25T07:43:21.997090"
+  },
+  {
+    "id": "1636f71d-9804-43b0-bdb0-00bc51ede9ba",
+    "title": "cafe20001-0218.mkv",
+    "status": "UPLOADING",
+    "createdAt": "2026-05-25T07:22:12.654669",
+    "updatedAt": "2026-05-25T07:22:31.238531"
+  },
+  {
+    "id": "35ab47f0-42e4-4cc3-aec5-88c010e51624",
+    "title": "bebebe",
+    "status": "UPLOADED",
+    "createdAt": "2026-05-16T15:32:43.271517",
+    "updatedAt": "2026-05-16T15:32:52.369733"
+  },
+  {
+    "id": "8820b686-bbb0-4918-9f86-42bcba6cabce",
+    "title": "bebebe",
+    "status": "PROCESSING_MEDIA",
+    "createdAt": "2026-05-07T11:58:50.837857",
+    "updatedAt": "2026-05-07T11:58:50.837857"
+  },
+  {
+    "id": "0ce15ef9-1904-46dd-b53f-53b43e1aeb01",
+    "title": "bebebe",
+    "status": "PROCESSING_ML",
+    "createdAt": "2026-05-05T07:10:38.126477",
+    "updatedAt": "2026-05-05T07:15:00.742423"
+  },
+  {
+    "id": "2636f71d-9804-43b0-bdb0-00bc51ede9bb",
+    "title": "cafe20001-0218.mkvcafe20001-0218.mkvcafe20001-0218.mkvcafe20001-0218.mkvcafe20001-0218.mkvcafe20001-0218.mkv",
+    "status": "READY",
+    "createdAt": "2026-05-25T07:22:12.654669",
+    "updatedAt": "2026-05-25T07:22:31.238531"
+  }
+]
---
+++ frontend/src/utils/parseDate.ts
@@ -0,0 +1,8 @@
+export const parseDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('ru-RU', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+};
