f7e925e91f80801d20ba4b212212c76223464777
ischademadda <156543642+ischademadda@users.noreply.github.com>
2026-06-04 20:22:04 +0700

    Revert "fix: properly configure formFieldLimit for Ktor file upload (#62)"
    This reverts commit adf2f2073c1b6069f791757c72770a441e6dc5f8.

--- backend/src/main/kotlin/com/v39a/omni/feature/video/api/VideoRoutes.kt
+++ backend/src/main/kotlin/com/v39a/omni/feature/video/api/VideoRoutes.kt
@@ -50,7 +50,10 @@ fun Route.videoRoutes() {
 
             // POST /upload
             post("/upload") {
-                val multipart = call.receiveMultipart(formFieldLimit = 500L * 1024 * 1024 * 1024)
+                //todo разобраться как его отключить или настроить иным образом, чтобы не было неявных лимитов
+                call.formFieldLimit = 500L * 1024 * 1024 * 1024
+
+                val multipart = call.receiveMultipart()
                 var uploadResponse: UploadResponse? = null
                 logger.info("received file upload")
                 var title = ""
